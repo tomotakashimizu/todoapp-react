b37abafb9d2bfc472e628539a22d7458b9f8bae5
tomotakashimizu <tomotaka.developer@gmail.com>
2022-05-28 16:29:17 +0900

    remove: delete unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
