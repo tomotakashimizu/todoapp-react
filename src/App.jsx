60feddb75088ce2c3bf432ef207a5adce2249644
tomotakashimizu <tomotaka.developer@gmail.com>
2022-05-29 02:04:29 +0900

    update: create UI for the app
    CSS Grid Layout を極める！（基礎編）
    https://qiita.com/kura07/items/e633b35e33e43240d363
    
    CSS Grid Layout を極める！（場面別編）
    https://qiita.com/kura07/items/486c19045aab8090d6d9

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,45 @@ import './App.css';
 export const App = () => {
   return (
     <>
-      <div>
-        <input placeholder='TODOを入力' />
+      <h1>ToDoApp-React</h1>
+      <div id='flex-container'>
+        <main id='grid-container'>
+          <div id='input-area'>
+            <input placeholder='TODOを入力' />
+            <button>追加</button>
+          </div>
+          <div id='incomplete-area'>
+            <p className='title'>未完了のTODO</p>
+            <ul>
+              <li>
+                <div className='list-row'>
+                  <p>aaa</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+              <li>
+                <div className='list-row'>
+                  <p>bbb</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div id='complete-area'>
+            <p className='title'>完了したTODO</p>
+            <ul>
+              <li>
+                <div className='list-row'>
+                  <p>ccc</p>
+                  <button>戻す</button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </main>
       </div>
-      <div></div>
-      <div></div>
     </>
   );
 };
