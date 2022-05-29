fdb7e012fe297af955fd8c48c8cf3f15c6e0afb3
tomotakashimizu <tomotaka.developer@gmail.com>
2022-05-29 16:45:38 +0900

    clean: componentize InputTodo css

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,10 +1,20 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const style = {
+  gridArea: 'input',
+  height: '30px',
+  textAlign: 'center',
+  backgroundColor: '#c1ffff',
+  borderRadius: '8px',
+  padding: '8px',
+  margin: '8px',
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div id='input-area'>
+    <div style={style}>
       <input placeholder='TODOを入力' value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
